33b0552521991ac92022929ddd0ab266eb6010c4
jonnyAnd <jonny@eino.co.uk>
2024-06-19 23:12:24 +0100

    template

--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
 function onLoad(){
-    console.log("hello");
+
+
+    hello();
+}
+
+
+function hello(){
+
+    console.log("hello-asd");
+
+    var thing = "string";
+
+    var i = 1;
+
+    var newString = returnAString();
+
+
+    for(var i=0 ; i<100 ; i++){
+        console.log(i);
+    }
+
+
+    var boolTing = "true";
+    var boolTing = true;
+    var boolTing = 1;
+
+    if(boolTing === true){
+
+    }
+
+    boolTing = true
+    boolTing == true
+    boolTing === true
+
+    
+
+    
+
+
+}
+
+
+function returnAString(){
+
+    var aString = "blah";
+
+    return aString;
+
+
 }
