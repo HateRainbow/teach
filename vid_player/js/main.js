d4e0d6a7156a63c51068c75873fe9baadb7213db
Pial Al Mamun <145979067+HateRainbow@users.noreply.github.com>
2024-07-04 22:42:06 +0200

    main.js

---
+++ vid_player/js/main.js
@@ -0,0 +1,14 @@
+var videoList = [
+  //   "https://www.youtube.com/embed/J2mRJ2RLhVE?si=b-Ci6CYrk8CVd6Pe",
+  //   "https://youtu.be/_0HTwQjMr9k?si=AqFvtCYODSmsOB3W ",
+  //   "https://youtu.be/g10V76ZoSYQ?si=LU-YzrQLEs1Wd64o",
+  //   "https://www.youtube.com/watch?v=aRPMG1PW330",
+  1, 2, 3, 4, 5,
+];
+
+function forward() {
+  videoList.splice();
+  videoList.push();
+}
+
+function backward() {}
